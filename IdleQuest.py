ee796053133b5ebad506600e2cf1133bff72a286
Jim Hessin <jhessin@gmail.com>
2026-04-30 16:18:59 -0600

    Set up a single clicking system.

--- IdleQuest.py
+++ IdleQuest.py
@@ -3,6 +3,7 @@ This is a clicking helper for idle quest.
 """
 import asyncio
 from time import sleep
+from threading import Timer
 
 from ahk import AHK
 
@@ -16,8 +17,18 @@ ahk.set_coord_mode('Mouse', 'Window')
 is_clicking = False
 is_moving = False
 
-def click_n_return(x: int, y: int) -> None:
-	global is_clicking, is_moving
+run_once = {
+	'click_n_return': False,
+	'toggle_click': False,
+	'reload_level': False,
+}
+
+
+def click_n_return(x: int, y: int, hotkey: str) -> None:
+	global is_clicking, is_moving, run_once
+	if run_once['click_n_return']:
+		return
+	run_once['click_n_return'] = True
 	was_clicking = False
 	was_moving = False
 	if is_clicking:
@@ -33,56 +44,42 @@ def click_n_return(x: int, y: int) -> None:
 		is_clicking = True
 	if was_moving:
 		is_moving = True
+	ahk.key_wait(hotkey, logical_state=True, released=True)
+	run_once['click_n_return'] = False
 
 
-def toggle_moving():
-	global is_moving, is_clicking
+def toggle_moving(hotkey: str):
+	global is_moving, is_clicking, run_once
 	is_moving = not is_moving
 	is_clicking = is_moving
 
 
-async def main_loop():
-	global is_clicking, is_moving
-	is_running = False
-	while True:
-		if ahk.win_is_active(title='Idlequest'):
-			if not is_running:
-				ahk.start_hotkeys()
-				is_running = True
-			if is_clicking:
-				ahk.show_tooltip('Clicking!!',0,0)
-				ahk.click()
-			if is_moving:
-				"""TODO: Implement mouse moving here"""
-		else:
-			if is_clicking:
-				toggle_click()
-			if is_moving:
-				toggle_moving()
-			if is_running:
-				ahk.stop_hotkeys()
-				is_running = False
-
-
-def toggle_click():
-	global is_clicking
+def toggle_click(hotkey: str):
+	global is_clicking, run_once
+	if run_once['toggle_click']:
+		return
+	run_once['toggle_click'] = True
 	is_clicking = not is_clicking
 	if not is_clicking:
 		ahk.show_tooltip('stopped clicking!', 0, 0)
-		sleep(5)
-		ahk.hide_tooltip()
+		Timer(5, ahk.hide_tooltip).start()
+	ahk.key_wait(hotkey, logical_state=True, released=True)
+	run_once['toggle_click'] = False
 
 
-def prev_level():
-	click_n_return(26, 70)
+def prev_level(hotkey: str):
+	click_n_return(26, 70, hotkey)
 
 
-def next_level():
-	click_n_return(236, 64)
+def next_level(hotkey: str):
+	click_n_return(236, 64, hotkey)
 
 
-def reload_level():
-	global is_clicking
+def reload_level(hotkey: str):
+	global is_clicking, run_once
+	if run_once['reload_level']:
+		return
+	run_once['reload_level'] = True
 	was_clicking = False
 	if is_clicking:
 		was_clicking = True
@@ -93,21 +90,43 @@ def reload_level():
 	ahk.mouse_position = home.x, home.y
 	if was_clicking:
 		is_clicking = True
+	ahk.key_wait(hotkey, logical_state=True, released=True)
+	run_once['reload_level'] = False
 
 
-# Use this for testing
-# ahk.add_hotkey('F1', reload_level)
+async def main_loop():
+	global is_clicking, is_moving, run_once
+	is_running = False
+	while True:
+		if ahk.win_is_active(title='Idlequest'):
+			if not is_running:
+				ahk.start_hotkeys()
+				is_running = True
+			if is_clicking:
+				ahk.show_tooltip('Clicking!!', 0, 0)
+				ahk.click()
+			if is_moving:
+				"""TODO: Implement mouse moving here"""
+		else:
+			if is_clicking:
+				toggle_click('')
+			if is_moving:
+				toggle_moving('')
+			if is_running:
+				ahk.stop_hotkeys()
+				is_running = False
+
 
 # Assign all hotkeys here
-ahk.add_hotkey('1', callback=toggle_click)
-ahk.add_hotkey('NUMPAD1', callback=toggle_click)
-ahk.add_hotkey('space', callback=toggle_click)
-ahk.add_hotkey('2', callback=prev_level)
-ahk.add_hotkey('NUMPAD2', callback=prev_level)
-ahk.add_hotkey('3', callback=next_level)
-ahk.add_hotkey('NUMPAD3', callback=next_level)
-ahk.add_hotkey('4', callback=reload_level)
-ahk.add_hotkey('NUMPAD4', callback=reload_level)
+ahk.add_hotkey('1', callback=lambda k='1': toggle_click(k))
+ahk.add_hotkey('NUMPAD1', callback=lambda k='NUMPAD1': toggle_click(k))
+ahk.add_hotkey('space', callback=lambda k='space': toggle_click(k))
+ahk.add_hotkey('2', callback=lambda k='2': prev_level(k))
+ahk.add_hotkey('NUMPAD2', callback=lambda k='NUMPAD2': prev_level(k))
+ahk.add_hotkey('3', callback=lambda k='3': next_level(k))
+ahk.add_hotkey('NUMPAD3', callback=lambda k='NUMPAD3': next_level(k))
+ahk.add_hotkey('4', callback=lambda k='4': reload_level(k))
+ahk.add_hotkey('NUMPAD4', callback=lambda k='NUMPAD4': reload_level(k))
 
 # Run the main loop
 asyncio.run(main_loop())
